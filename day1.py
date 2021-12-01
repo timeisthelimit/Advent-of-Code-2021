aa8ed050b4ac6076e6f4bee5be087a6d3d5367d0
Oskar Aleksander (The Great) Jankowski <oskar.jankowski.oj@gmail.com>
2021-12-01 06:21:35 +0000

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,32 @@
+with open("input.in", 'r') as f:
+    last = f.readline()
+    count = 0
+    while (line := f.readline()) != '': 
+        if line > last:
+            count += 1
+
+        print(last, line, count)
+        print()
+        last = line
+    print("part 1: ", count)
+
+with open("input.in", 'r') as f:
+    count = 0
+    a = f.readline()
+    b = f.readline()
+    c = f.readline()
+
+    lastsum = int(a)+int(b)+int(c)
+
+    while (line := f.readline()) != '': 
+
+        a = b
+        b = c
+        c = line
+
+        if (int(a)+int(b)+int(c)) > lastsum:
+            count+=1
+
+        lastsum = int(a)+int(b)+int(c)
+    print("part 2: ", count)
+
